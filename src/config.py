0bcd09a1554931d6ab32bb885081e6b8157aebba
njguerram <njguerram@unal.edu.co>
2025-11-11 11:01:39 -0500

    Cambios para la optimización del código

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ load_dotenv("dot_env_content.env")
 
 # === Paths base ===
 INPUTS_PATH = os.getenv("INPUTS_PATH")
-AOI_DIR = os.path.join(INPUTS_PATH, "dynamic_world", "area_estudio")
+AOI_DIR = os.path.join(INPUTS_PATH, "area_estudio", "dynamic_world")
 OUTPUTS_BASE = os.path.join(INPUTS_PATH, "dynamic_world", "outputs")
 LOGO_PATH = os.path.join(INPUTS_PATH, "Logo_SDP.jpeg")
 
